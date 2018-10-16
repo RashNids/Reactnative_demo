3d9d6f38c48bc2419375a906a1d585d9c578f734
RashNids <rashmikhandelwal936@gmail.com>
2018-10-16 20:03:51 +0530

    push code

---
+++ index.js
@@ -0,0 +1,19 @@
+//Import a library to help create a component
+
+import React from 'react';
+import { /*Text,*/ AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//create a component
+const App = () => (
+     // <Text>Some Text</Text>
+     <View style={{ flex: 1 }}>
+     <Header headerText={'Albums'} />
+     <AlbumList />
+     </View>
+);
+
+
+//render it to device
+AppRegistry.registerComponent('albums', () => App);
